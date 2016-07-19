ce81418ece1eb9a73d3059bf5d2ea1e84f0cc638
bjw-fightword <641605665@qq.com>
2016-07-19 17:23:32 +0800

    video

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+	$("#controls-top").click(function(){
+		$("#controls-top .control-button.slide").slideToggle(400);
+	})
+})
+/*返回顶部*/
+$(window).scroll(function(){
+	if($(window).scrollTop()>100){
+		$(".return-top").fadeIn();
+	}else{
+		$(".return-top").fadeOut();
+	}
+	$(".return-top").click(function(){
+		$("body").animate({scrollTop:0});
+		$(document.documentElement).animate({scrollTop:0});
+	})
+})
